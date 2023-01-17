4b7c478816c74c28b435dd1300f5565e171e220f
Nahuel Barañado Mendez <nahuelbaranado@gmail.com>
2023-01-17 13:56:40 -0400

    final

---
+++ Source/Arcanoid/barrera.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "barrera.h"
+
+// Sets default values
+Abarrera::Abarrera()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void Abarrera::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void Abarrera::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
