c517e8c854e8b51b50bdda5c12e05680df4079ad
Nahuel Barañado Mendez <nahuelbaranado@gmail.com>
2023-01-24 10:01:24 -0400

    segundainstancia

---
+++ Source/Arcanoid/Director.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Director.h"
+
+// Add default functionality here for any IDirector functions that are not pure virtual.
